2112f198407c2243b4628c95e8c3e71e984b4e7c
root <root@yq01-qianmo-com-88-160-26.yq01.baidu.com>
2021-09-13 15:22:09 +0000

    add pretrain code

---
+++ contrib/layoutlm_pretrain/train_pretrain_ppxlm.sh
@@ -0,0 +1,19 @@
+# export CUDA_VISIBLE_DEVICES=5
+
+export PYTHONPATH=../../:$PYTHONPATH
+
+# python3.7 train_xlm.py \
+python3.7 -m paddle.distributed.launch --gpus '4,5,6,7' train_xlm.py \
+    --model_type "layoutxlm-pp" \
+    --model_name_or_path "./layoutxlm-pp-base-paddle/" \
+    --max_seq_length 512 \
+    --train_data_dir "dataset/" \
+    --train_label_path "dataset/json_all224.txt" \
+    --num_train_epochs 20 \
+    --eval_steps 10 \
+    --save_steps 30000 \
+    --output_dir "output_layoutxlm-pp_v1/" \
+    --learning_rate 5e-5 \
+    --warmup_steps 50 \
+    --per_gpu_train_batch_size 8 \
+    --per_gpu_eval_batch_size 8 \
